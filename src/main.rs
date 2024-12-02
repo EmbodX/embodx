c89116704a648e6543a823750ca9438dc1ddea52
Tin Lai <tin@tinyiu.com>
2024-12-02 14:19:16 +1000

    fix build
    Signed-off-by: Tin Lai <tin@tinyiu.com>

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use bevy::prelude::*;
 use dimensify::SimPlugin;
 use eyre::Result;
 
-mod web_demo;
+// mod web_demo;
 
-use dimensify::test_scene;
-use dimensify::util;
+// use dimensify::test_scene;
+// use dimensify::util;
 
 fn main() -> Result<()> {
-    util::initialise()?;
+    // util::initialise()?;
 
     let mut app = App::new();
     app.add_plugins(SimPlugin)
-        .add_plugins(test_scene::plugin)
+        // .add_plugins(test_scene::plugin)
         .run();
 
     Ok(())
